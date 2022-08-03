014a08f10a400f523b561aabf4abe0e26eb3fc28
Karolina Staryszak <you@example.com>
2022-08-03 13:50:34 +0200

    Dodano app.MapPost

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,6 +1,11 @@
 ﻿var builder = WebApplication.CreateBuilder();
+//builder.Logging.AddJsonConsole();
+//builder.Services.AddScoped<>();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World");
+app.MapPost("/", () => "This is a POST");
+//app.MapGet("/", () => "Hello World");
 app.MapGet("/bye", () => "Goodbye World");
 app.Run();

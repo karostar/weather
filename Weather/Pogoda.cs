7431198ed1fc5277ab80817c52cb19f57377fb49
Karolina Staryszak <you@example.com>
2022-08-03 14:14:07 +0200

    Dodano klase Pogoda

---
+++ Weather/Pogoda.cs
@@ -0,0 +1,9 @@
+﻿namespace Weather
+{
+    public class Pogoda
+    {
+        public DateTime data { get; set; }
+
+        public int temperatura { get; set; }
+    }
+}
--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,11 +1,16 @@
 ﻿var builder = WebApplication.CreateBuilder();
 //builder.Logging.AddJsonConsole();
 //builder.Services.AddScoped<>();
-
-
 var app = builder.Build();
 
-app.MapPost("/", () => "This is a POST");
+var pogoda = new Weather.Pogoda
+{
+    data = DateTime.Now,
+    temperatura = 29
+};
+
+//app.MapPost("/", () => "This is a POST");
 //app.MapGet("/", () => "Hello World");
+app.MapGet("/", () => pogoda);
 app.MapGet("/bye", () => "Goodbye World");
 app.Run();

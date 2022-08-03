14816c153923f8d93ff2165423af460d9d17f10a
Karolina Staryszak <you@example.com>
2022-08-03 14:42:34 +0200

    Dodano serializacje

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,4 +1,6 @@
-﻿var builder = WebApplication.CreateBuilder();
+﻿using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder();
 //builder.Logging.AddJsonConsole();
 //builder.Services.AddScoped<>();
 var app = builder.Build();
@@ -9,8 +11,16 @@ var pogoda = new Weather.Pogoda
     temperatura = 29
 };
 
+string jsonString = JsonSerializer.Serialize(pogoda);
+
+Weather.Pogoda pogoda1 =
+                JsonSerializer.Deserialize<Weather.Pogoda>(jsonString);
+
+//Console.WriteLine($"Data: {pogoda1.data}");
+//Console.WriteLine($"Temperatura: {pogoda1.temperatura}");
+
 //app.MapPost("/", () => "This is a POST");
 //app.MapGet("/", () => "Hello World");
-app.MapGet("/", () => pogoda);
+app.MapGet("/", () => pogoda1.data+ Environment.NewLine + pogoda1.temperatura+" stopni Celsjusza");
 app.MapGet("/bye", () => "Goodbye World");
 app.Run();

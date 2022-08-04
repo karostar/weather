51630fb271918113ae456bf1311806b3aff2a0db
Karolina Staryszak <you@example.com>
2022-08-04 10:04:18 +0200

    Drobne poprawki

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -1,23 +1,24 @@
 ﻿using System.Text.Json;
+using Weather;
 
 var builder = WebApplication.CreateBuilder();
 //builder.Logging.AddJsonConsole();
 //builder.Services.AddScoped<>();
 var app = builder.Build();
 
-List<Weather.Pogoda> pogoda = new List<Weather.Pogoda>
+List<Pogoda> pogoda = new List<Pogoda>
 {
-    new Weather.Pogoda{data=DateTime.Now, temperatura=22},
-    new Weather.Pogoda{data=DateTime.Now.AddDays(1), temperatura=26},
-    new Weather.Pogoda{data=DateTime.Now.AddDays(2), temperatura=21}
+    new Pogoda{data=DateTime.Now, temperatura=22},
+    new Pogoda{data=DateTime.Now.AddDays(1), temperatura=26},
+    new Pogoda{data=DateTime.Now.AddDays(2), temperatura=21}
 };
 
 //string jsonString = JsonSerializer.Serialize(pogoda);
 
 //Weather.Pogoda pogoda1 = JsonSerializer.Deserialize<Weather.Pogoda>(jsonString);
 
-//app.MapPost("/", () => "This is a POST");
 app.MapGet("/", () => pogoda);
+app.MapPost("/add", (Pogoda p) => "$Dodano nową prognozę");
 //app.MapGet("/", () => pogoda[1].data + Environment.NewLine + pogoda[1].temperatura + " stopni Celsjusza");
 app.MapGet("/bye", () => "Goodbye World");
 app.Run();

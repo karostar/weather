d175ba30fd8247cce72d8348931f279a511ebaf6
Karolina Staryszak <you@example.com>
2022-08-04 09:05:31 +0200

    Dodano liste

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -5,20 +5,19 @@ var builder = WebApplication.CreateBuilder();
 //builder.Services.AddScoped<>();
 var app = builder.Build();
 
-var pogoda = new Weather.Pogoda
+List<Weather.Pogoda> pogoda = new List<Weather.Pogoda>
 {
-    data = DateTime.Now,
-    temperatura = 29
+    new Weather.Pogoda{data=DateTime.Now, temperatura=22},
+    new Weather.Pogoda{data=DateTime.Now.AddDays(1), temperatura=26},
+    new Weather.Pogoda{data=DateTime.Now.AddDays(2), temperatura=21}
 };
 
 //string jsonString = JsonSerializer.Serialize(pogoda);
 
 //Weather.Pogoda pogoda1 = JsonSerializer.Deserialize<Weather.Pogoda>(jsonString);
 
-//Console.WriteLine($"Data: {pogoda1.data}");
-//Console.WriteLine($"Temperatura: {pogoda1.temperatura}");
-
 //app.MapPost("/", () => "This is a POST");
-app.MapGet("/", () => pogoda.data + Environment.NewLine + pogoda.temperatura + " stopni Celsjusza");
+app.MapGet("/", () => pogoda);
+//app.MapGet("/", () => pogoda[1].data + Environment.NewLine + pogoda[1].temperatura + " stopni Celsjusza");
 app.MapGet("/bye", () => "Goodbye World");
 app.Run();

a0873c1098fcb377846360fd0d74a39315cb9708
Marcin <mwozniak@teldat.local>
2022-08-05 15:25:38 +0200

    Duża dawka wiedzy :-)

--- Weather/Program.cs
+++ Weather/Program.cs
@@ -4,33 +4,64 @@ using Weather;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 
+
+
+
+// Do poczytania: argumenty generyczne w typach klas, typ List<T>, Dictionary<T,K> i jak ich używamy
+var colors = new Dictionary<string, string>();
+colors.Add("red", "czerwony");
+colors.Add("blue", "niebieski");
+
+if (colors.ContainsKey("red"))
+{
+    Console.WriteLine(colors["red"]);
+}
+
+// Klasa Type
+Type mojaKlasa = typeof(Dictionary<string, string>);
+Console.WriteLine(mojaKlasa.Name);
+
+var kontenerDI = new Dictionary<Type, object>(); // wartość musi dziedziczyć, implementować lub być typu klucz
+kontenerDI.Add(typeof(IMeasurementSource), new RandomMeasurementSource());
+
+var source = (IMeasurementSource) kontenerDI[typeof(IMeasurementSource)];
+
+var numOfColors = new Dictionary<string, int>();
+numOfColors["blue"] = 1;
+
+var test = numOfColors["blue"] + 1;
+
+
+// Do poczytania: interfejsy
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddDbContext<WeatherMeasurementsDb>(options => options.UseInMemoryDatabase("results"));
+builder.Services.AddSingleton<IMeasurementSource, NullMeasurementSource>();
 var app = builder.Build();
 
 Random rand = new Random();
 
-// Test
-//app.MapGet("/weather", async (WeatherMeasurementsDb db) => await db.weatherMeasurements.Select(x => new WeatherMeasurementsDTO(x)).ToListAsync()); //returns all entries
-
-app.MapGet("/weather", async (WeatherMeasurementsDb db, [FromQuery] DateTime? from) => { //returns entries by full date
+// Fajnie jak adres wskazuje nam konkretny zasób
+// Dodajmy filtrowanie w przedziale czasu (from, to)
+app.MapGet("/weather/measurements", async (WeatherMeasurementsDb db, [FromQuery] DateTime? from) => { 
+    //returns entries by full date
     var data = from WeatherMeasurementsDb in db.weatherMeasurements
                where !@from.HasValue || WeatherMeasurementsDb.date > @from select WeatherMeasurementsDb;
     return data;
 });
 
-app.MapGet("/weather/current", async (WeatherMeasurementsDb db) => {  //creates a new entry
-    DateTime dateTime = DateTime.Now;
-    WeatherMeasurements w = new WeatherMeasurements();
-    w.date = dateTime;
-    w.temperatureC = rand.Next(-20, 50);
+// Dodajmy adres dla pojedynczego odczytu: /weather/measurements/{id}
 
+
+
+app.MapGet("/weather/measurements/current", async (IMeasurementSource source, WeatherMeasurementsDb db) => {  //creates a new entry
+    var w = source.GetCurrentMeasurement();
     await db.weatherMeasurements.AddAsync(w);
     await db.SaveChangesAsync();
     return Results.Created($"/weather/{w.Id}", w);
 });
 
-app.MapGet("/weather/add", async (WeatherMeasurementsDb db) => {  //creates a new entry
+
+app.MapGet("/weather/measurements/add", async (WeatherMeasurementsDb db) => {  //creates a new entry
     int year = rand.Next(2005, 2022);
     int month = rand.Next(1, 12);
     int day = rand.Next(1, 30);
--- Weather/WeatherMeasurements.cs
+++ Weather/WeatherMeasurements.cs
@@ -1,5 +1,38 @@
 ﻿namespace Weather
 {
+    public interface IMeasurementSource
+    {
+        WeatherMeasurements GetCurrentMeasurement();
+    }
+
+    public class RandomMeasurementSource : IMeasurementSource
+    {
+        public string Name => "Tralalala";
+
+        public WeatherMeasurements GetCurrentMeasurement()
+        {
+            Random rand = new Random();
+            DateTime dateTime = DateTime.Now;
+            WeatherMeasurements w = new WeatherMeasurements();
+            w.date = dateTime;
+            w.temperatureC = rand.Next(-20, 50);
+            return w;
+        }
+    }
+
+    public class NullMeasurementSource : IMeasurementSource
+    {
+        public WeatherMeasurements GetCurrentMeasurement()
+        {
+            WeatherMeasurements w = new WeatherMeasurements();
+            w.temperatureC = 0;
+            return w;
+        }
+    }
+
+
+    // Starajmy się być konsekwentni w nazywaniu i stosujmy sie do sugestii nazewnictwa
+    // https://docs.microsoft.com/pl-pl/dotnet/csharp/fundamentals/coding-style/coding-conventions
     public class WeatherMeasurements
     {
         public int Id { get; set; }
